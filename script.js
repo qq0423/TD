3bf0eae68e664d66ed9016afbb8ff42f79acb405
qq0423 <152591253+qq0423@users.noreply.github.com>
2023-12-01 17:52:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let ws = new WebSocket("wss://websocket-p75n.onrender.com/:443");
+let ws = new WebSocket("wss://two0231201.onrender.com");
 
 let controllTD = document.querySelector('.controllTD') ;
 controllTD.addEventListener('input', (event) => {
